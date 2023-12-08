4e69d8cec10fc726e69fbd44f9a7e2beeb358c9d
Frederik Rasmussen <wtfhaxxorone@gmail.com>
2023-12-08 14:51:56 +0100

    Tilføjet navbar, og undersider. <nav> links i shared/_layout skal laves om

---
+++ Joga/Pages/Priser.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Joga.Pages
+{
+    public class PriserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
