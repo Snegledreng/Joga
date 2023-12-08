ddb772947099c9f9b1b0da3e32dd8166794ec4e4
Søren Vinther Hansen <soh003@edu.zealand.dk>
2023-12-08 12:14:37 +0100

    add medlemrepo

---
+++ Joga/services/MedlemRepository.cs
@@ -0,0 +1,17 @@
+﻿using Joga.model;
+
+namespace Joga.services
+{
+    public class MedlemRepository
+    {
+        //Instansfelt og properties(Get/Set)
+        public List<Medlem> MedlemList { get; set; }
+
+        //Konstruktør
+        public MedlemRepository()
+        {
+            MedlemList = new List<Medlem>();
+        }
+
+    }
+}
