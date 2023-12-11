ad2b4e5ce20a4cab07f507187e787de3a102df6d
Søren Vinther Hansen <soh003@edu.zealand.dk>
2023-12-11 10:05:15 +0100

    Opret medlem bind property .cs

--- Joga/Pages/OpretMedlem.cshtml.cs
+++ Joga/Pages/OpretMedlem.cshtml.cs
@@ -5,6 +5,25 @@ namespace Joga.Pages
 {
     public class OpretMedlemModel : PageModel
     {
+        [BindProperty]
+        public int NyId { get; set; }
+
+        [BindProperty]
+        public string NyNavn { get; set; }
+
+        [BindProperty]
+        public string NyEmail { get; set; }
+
+        [BindProperty]
+        public string NyTlf { get; set; }
+
+        [BindProperty]
+        public bool NyStatus { get; set; }
+
+        [BindProperty]
+        public bool NyNyhedsbrev { get; set; }
+
+
         public void OnGet()
         {
         }
